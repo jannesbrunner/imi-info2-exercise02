735f35ae3d6c5a1d25d0ae9801d038f52c0eb46a
Eli Gould <MariaKatharinaWunder@gmail.com>
2016-04-26 10:32:06 +0200

    Create read file method

--- src/excercise02/TxtAnalyser.java
+++ src/excercise02/TxtAnalyser.java
@@ -3,21 +3,32 @@
  */
 package excercise02;
 
+import java.io.*;
+
 /**
  * @author Jannes Brunner
  *
  */
 public class TxtAnalyser {
 
+	FileReader fr;
+	BufferedReader br; 
+	char[] charArray;
 	
-	
-	
-	
-	public TxtAnalyser() {
-		
+	public TxtAnalyser() throws IOException {
+		readFile();
 	}
 	
-	public static void main(String[] args) {
+	public void readFile() throws IOException{
+		fr = new FileReader("file.txt");
+		br = new BufferedReader(fr);
+		br.read(charArray);
+		for(char test : charArray){
+			System.out.println(test);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
 		new TxtAnalyser();
 	}
 
