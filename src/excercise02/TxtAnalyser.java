6fac92a2880dd57ca80850fb1cd72bae354dafbc
jannesbrunner <jannes.brunner@students.htw-berlin.de>
2016-04-27 13:39:26 +0200

    Add filewriting ability

--- src/excercise02/TxtAnalyser.java
+++ src/excercise02/TxtAnalyser.java
@@ -14,70 +14,89 @@ import java.util.ArrayList;
 public class TxtAnalyser {
 
 	FileReader fr;
-	BufferedReader br; 
+	BufferedReader br;
+	FileWriter fw;
+	BufferedWriter bw;
 	ArrayList<Character> charArrayList;
-	String output;
-	int[] statistic; 
-	
+	String outputnative;
+	String outputstatistics;
+	int[] statistic;
+
 	public TxtAnalyser() throws IOException {
 		statistic = new int[94];
-		output = "";
+		outputnative = "";
 		charArrayList = new ArrayList<Character>();
 		readFile();
-		output();
 		analysis();
-		
-		
-		
-	}
+		output();
+		fileoutput();
 	
-	public void readFile() throws IOException{
-		
+
+	}
+
+	public void readFile() throws IOException {
+
 		fr = new FileReader("file.txt");
 		br = new BufferedReader(fr);
-		
+
 		int line = 0;
-		
+
 		while ((line = br.read()) != -1) {
 			char actualChar = (char) line;
-			output += actualChar;
-			if (actualChar > 33 && actualChar < 132){
+			outputnative += actualChar;
+			if (actualChar > 33 && actualChar < 132 ) {
 				charArrayList.add(actualChar);
 			}
 		}
 
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		new TxtAnalyser();
 	}
-	
-	public void output(){
-		System.out.println(output);
+
+	public void output() {
+		System.out.println("Input text:");
+		System.out.println(outputnative);
+		System.out.println(outputstatistics);
 		System.out.println(charArrayList.toString());
 	}
-	
-	public void analysis(){
-		
-		for (char toAnalyse : charArrayList){
-			
+
+	public void analysis() {
+
+		for (char toAnalyse : charArrayList) {
+
 			int toAdd = (int) toAnalyse - 33;
 			statistic[toAdd] += 1;
 		}
-		for (int i = 0; i < statistic.length; i++){
-		    
+		String nl = System.getProperty("line.seperator");
+		this.outputstatistics = "\n Statistic for your input: \n";
+		for (int i = 0; i < statistic.length; i++) {
+
 			char temp = (char) (i + 32);
 			String tempString = "";
-			for (int count = 0; count < statistic[i]; count++){
+			
+			for (int count = 0; count < statistic[i]; count++) {
 				tempString += "#";
 			}
-			System.out.println(temp + ":" + statistic[i] + tempString);
 			
+			this.outputstatistics += (temp + ":" + statistic[i] + tempString +"\n");
+
 		}
 	}
 	
-	
-	
+	public void fileoutput() throws IOException {
+		File outputfile = new File("output.txt");
+		fw = new FileWriter(outputfile);
+		bw = new BufferedWriter(fw);
+		
+		bw.write("Input was: \n");
+		bw.newLine();
+		bw.write(outputnative);
+		bw.newLine();
+		bw.write(outputstatistics);
+		
+		bw.close();
+	}
 
 }
-
